b83418a58ce9f09c24112e5fdec5bd98e167e75b
CAGS295 <33366442+CAGS295@users.noreply.github.com>
2017-11-15 16:59:29 -0600

    Update V1.R

--- V1.R
+++ V1.R
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 
 ##### Paqueterias
 pkg <- c("base","downloader","dplyr","fBasics","forecast","grid",
@@ -16,28 +14,3 @@ instpackages <- lapply(pkg, library, character.only=TRUE)
 RawGitHub <- "https://raw.githubusercontent.com/IFFranciscoME/"
 ROandaAPI <- paste(RawGitHub,"ROandaAPI/master/ROandaAPI.R",sep="")
 downloader::source_url(ROandaAPI,prompt=FALSE,quiet=TRUE)
-
-jguyuyyuly<-2
-
-=======
-
-
-##### Paqueterias
-pkg <- c("base","downloader","dplyr","fBasics","forecast","grid",
-         "gridExtra","httr","jsonlite","lmtest","lubridate","moments",
-         "matrixStats", "PerformanceAnalytics","plyr","plotly","quantmod",
-         "reshape2","RCurl", "stats","scales","tseries",
-         "TTR","TSA","XML","xts","zoo","yaml")
-
-inst <- pkg %in% suppressMessages(installed.packages())
-if(length(pkg[!inst]) > 0) install.packages(pkg[!inst])
-instpackages <- lapply(pkg, library, character.only=TRUE)
-
-#####
-RawGitHub <- "https://raw.githubusercontent.com/IFFranciscoME/"
-ROandaAPI <- paste(RawGitHub,"ROandaAPI/master/ROandaAPI.R",sep="")
-downloader::source_url(ROandaAPI,prompt=FALSE,quiet=TRUE)
-
-
-hiii<-34
-khhkk<- 78
