37fadf7e81965eb5ce84dcf6dcf315f1a97bdcd4
CAGS295 <33366442+CAGS295@users.noreply.github.com>
2017-11-15 15:30:17 -0600

    Add files via upload

---
+++ V1.R
@@ -0,0 +1,17 @@
+
+
+##### Paqueterias
+pkg <- c("base","downloader","dplyr","fBasics","forecast","grid",
+         "gridExtra","httr","jsonlite","lmtest","lubridate","moments",
+         "matrixStats", "PerformanceAnalytics","plyr","plotly","quantmod",
+         "reshape2","RCurl", "stats","scales","tseries",
+         "TTR","TSA","XML","xts","zoo","yaml")
+
+inst <- pkg %in% suppressMessages(installed.packages())
+if(length(pkg[!inst]) > 0) install.packages(pkg[!inst])
+instpackages <- lapply(pkg, library, character.only=TRUE)
+
+#####
+RawGitHub <- "https://raw.githubusercontent.com/IFFranciscoME/"
+ROandaAPI <- paste(RawGitHub,"ROandaAPI/master/ROandaAPI.R",sep="")
+downloader::source_url(ROandaAPI,prompt=FALSE,quiet=TRUE)
