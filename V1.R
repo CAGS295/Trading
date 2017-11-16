899063043c358175121b7341ca7bf6a7bdcb7d31
CAGS295 <calgusa@gmail.com>
2017-11-15 19:23:54 -0600

    Avance 2

--- V1.R
+++ V1.R
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-
 ##### Paqueterias
 pkg <- c("base","downloader","dplyr","fBasics","forecast","grid",
          "gridExtra","httr","jsonlite","lmtest","lubridate","moments",
@@ -18,102 +14,33 @@ RawGitHub <- "https://raw.githubusercontent.com/IFFranciscoME/"
 ROandaAPI <- paste(RawGitHub,"ROandaAPI/master/ROandaAPI.R",sep="")
 downloader::source_url(ROandaAPI,prompt=FALSE,quiet=TRUE)
 
-<<<<<<< HEAD
-et
-
->>>>>>> 51b12c338c93e3bd4ec77839629f5efbc35c1177
->>>>>>> 5020a004c143464d8e9ea6ee1a36e8d12d3e5546
-=======
-<<<<<<< HEAD
-
-
-##### Paqueterias
-pkg <- c("base","downloader","dplyr","fBasics","forecast","grid",
-         "gridExtra","httr","jsonlite","lmtest","lubridate","moments",
-         "matrixStats", "PerformanceAnalytics","plyr","plotly","quantmod",
-         "reshape2","RCurl", "stats","scales","tseries",
-         "TTR","TSA","XML","xts","zoo","yaml")
-
-inst <- pkg %in% suppressMessages(installed.packages())
-if(length(pkg[!inst]) > 0) install.packages(pkg[!inst])
-instpackages <- lapply(pkg, library, character.only=TRUE)
-
+type <- "practice"    # Account Type
+ID <- "1742531"     # Account ID
+token <- "ada4a61b0d5bc0e5939365e01450b614-4121f84f01ad78942c46fc3ac777baa6"
+inst<- "EUR_USD"
+Aux<-AccountInfo(OA_At,OA_Ai,OA_Ak)
+i=0;
+flag=TRUE;
+s=ActualPrice(type,token,"EUR_USD")
+Sys.sleep(5)
+while(flag){
+  i=i+1;
+  #s<- 
+    s=rbind(s,ActualPrice(type,token,"EUR_USD")) 
+    
+    ##rbind(s,)
+  Sys.sleep(5)
+}
 #####
-RawGitHub <- "https://raw.githubusercontent.com/IFFranciscoME/"
-ROandaAPI <- paste(RawGitHub,"ROandaAPI/master/ROandaAPI.R",sep="")
-downloader::source_url(ROandaAPI,prompt=FALSE,quiet=TRUE)
-
-jguyuyyuly<-2
-
-=======
 
 
-##### Paqueterias
-pkg <- c("base","downloader","dplyr","fBasics","forecast","grid",
-         "gridExtra","httr","jsonlite","lmtest","lubridate","moments",
-         "matrixStats", "PerformanceAnalytics","plyr","plotly","quantmod",
-         "reshape2","RCurl", "stats","scales","tseries",
-         "TTR","TSA","XML","xts","zoo","yaml")
-
-inst <- pkg %in% suppressMessages(installed.packages())
-if(length(pkg[!inst]) > 0) install.packages(pkg[!inst])
-instpackages <- lapply(pkg, library, character.only=TRUE)
-
-#####
-RawGitHub <- "https://raw.githubusercontent.com/IFFranciscoME/"
-ROandaAPI <- paste(RawGitHub,"ROandaAPI/master/ROandaAPI.R",sep="")
-downloader::source_url(ROandaAPI,prompt=FALSE,quiet=TRUE)
 
 
-hiii<-34
->>>>>>> 42e0bb371b2881e0ed696cd958ff3957230d10bd
-=======
-<<<<<<< HEAD
 
 
-##### Paqueterias
-pkg <- c("base","downloader","dplyr","fBasics","forecast","grid",
-         "gridExtra","httr","jsonlite","lmtest","lubridate","moments",
-         "matrixStats", "PerformanceAnalytics","plyr","plotly","quantmod",
-         "reshape2","RCurl", "stats","scales","tseries",
-         "TTR","TSA","XML","xts","zoo","yaml")
-
-inst <- pkg %in% suppressMessages(installed.packages())
-if(length(pkg[!inst]) > 0) install.packages(pkg[!inst])
-instpackages <- lapply(pkg, library, character.only=TRUE)
-
-#####
-RawGitHub <- "https://raw.githubusercontent.com/IFFranciscoME/"
-ROandaAPI <- paste(RawGitHub,"ROandaAPI/master/ROandaAPI.R",sep="")
-downloader::source_url(ROandaAPI,prompt=FALSE,quiet=TRUE)
-
-jguyuyyuly<-2
-
-=======
-
-=======
->>>>>>> b83418a58ce9f09c24112e5fdec5bd98e167e75b
-
-##### Paqueterias
-pkg <- c("base","downloader","dplyr","fBasics","forecast","grid",
-         "gridExtra","httr","jsonlite","lmtest","lubridate","moments",
-         "matrixStats", "PerformanceAnalytics","plyr","plotly","quantmod",
-         "reshape2","RCurl", "stats","scales","tseries",
-         "TTR","TSA","XML","xts","zoo","yaml")
-
-inst <- pkg %in% suppressMessages(installed.packages())
-if(length(pkg[!inst]) > 0) install.packages(pkg[!inst])
-instpackages <- lapply(pkg, library, character.only=TRUE)
-
-#####
-RawGitHub <- "https://raw.githubusercontent.com/IFFranciscoME/"
-ROandaAPI <- paste(RawGitHub,"ROandaAPI/master/ROandaAPI.R",sep="")
-downloader::source_url(ROandaAPI,prompt=FALSE,quiet=TRUE)
-<<<<<<< HEAD
 
 
-hiii<-34
-khhkk<- 78
->>>>>>> 88acfa4eb8b5f13fd9badac151d3048f9c56d6b4
-=======
->>>>>>> b83418a58ce9f09c24112e5fdec5bd98e167e75b
+
+
+
+
