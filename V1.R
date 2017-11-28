1f5c6a1d1d3949d7635c795ac3972d9197b1b4e6
CAGS295 <calgusa@gmail.com>
2017-11-28 11:36:24 -0600

    28/11/2017

--- V1.R
+++ V1.R
@@ -1,49 +1,55 @@
-##### Paqueterias
+
+##### 
 pkg <- c("base","downloader","dplyr","fBasics","forecast","grid",
          "gridExtra","httr","jsonlite","lmtest","lubridate","moments",
          "matrixStats", "PerformanceAnalytics","plyr","plotly","quantmod",
          "reshape2","RCurl", "stats","scales","tseries",
-         "TTR","TSA","XML","xts","zoo","yaml")
+         "TTR","TSA","XML","xts","zoo","yaml","data.table")
 
 inst <- pkg %in% suppressMessages(installed.packages())
 if(length(pkg[!inst]) > 0) install.packages(pkg[!inst])
 instpackages <- lapply(pkg, library, character.only=TRUE)
-
+source('fs.R')
+ #Paqueterias
 #####
-RawGitHub <- "https://raw.githubusercontent.com/IFFranciscoME/"
-ROandaAPI <- paste(RawGitHub,"ROandaAPI/master/ROandaAPI.R",sep="")
-downloader::source_url(ROandaAPI,prompt=FALSE,quiet=TRUE)
+# RawGitHub <- "https://raw.githubusercontent.com/IFFranciscoME/"
+# ROandaAPI <- paste(RawGitHub,"ROandaAPI/master/ROandaAPI.R",sep="")
+# downloader::source_url(ROandaAPI,prompt=FALSE,quiet=TRUE)
 
-#####
 #Params
-K=100000;tr_size=21;d=3;dd=5;sig=5;atr=5;thrsh=.075;upp_lim=35;low_lim=-35;flag=0;inst="USD_MXN";lever=50;exposure=.1*lever;
-grn="S5";align=17;tz="America/Mexico_City"
 #####
+K=100000;n=21;d=3;dd=5;sig=5;atr=5;thrsh=.0000075;upp_lim=35;low_lim=-35;flag=0;lever=50;exposure=.1*lever;
+grn="S5";align=17;tz="America/Mexico_City" 
 #Account
-
+##### 
 conf=yaml.load_file('OANDAkey.yaml')
 type=conf$ACC_TYPE
 ID= conf$ACC_ID
 token=conf$API_KEY_OANDA
-inst=conf$PAIR
-
+inst=conf$PAIR   
 #Aux<-AccountInfo(type,ID,token)
-
+#Model
 #####
+tr_size=n;
 flag1=TRUE;
+
 s=HisPrices(type,grn,align,tz,token,inst,Count = 1)
 while(flag1){
-  aux=HisPrices(type,grn,align,tz,token,inst,Count = 1)
-  if(s[1,1] != aux[1,1]){
+  tryCatch({aux=HPrice(type,grn,align,tz,token,inst,Count = 1)},error=next())
+  if(s[dim(s)[1],1] != aux[1,1]){
     s=rbind(s,aux)
-    if(dim(s)[1] == (tr_size+10)) {s=s[-dim(s)[1],]}
+    if(dim(s)[1] == (tr_size+1)) {s=s[-1,]}
     if(dim(s)[1]>=tr_size){ #modelo
-      idx=SMI(s[,c(3:5)],tr_size,d,dd,sig)
-      
+      atr_=ATR(s[,3:5],atr)[tr_size,2]
+      idx=SMI(s[,3:5],n,d,dd,sig)[(tr_size-3):tr_size,]
+      if(atr_>thrsh){
+        OrderHandler(STR1(idx), ...)
+      }
     }
   }
-}
+} 
 #####
+
 
 
 
--- fs.R
+++ fs.R
@@ -1,8 +1,180 @@
 
-STR1<- function(smi){
-  control = data.frame(smi,VC=NA,Cruce=NA,Intervalo=NA,Int_diff=NA,Posicion=NA)
+STR1<- function(smi,pos=1){
+  control = data.frame(smi,VC=NA,Cruce=NA,Intervalo=NA,Int_diff=NA)
   control$VC=ifelse((control$SMI-control$signal)>0, 1, 0)
   control$Cruce=c(NA,diff(control$VC))
   #control$Posicion[n]=0
-  return(control)
+  size=nrow(control)
+  for(i in 1:size){
+    
+    if(control$SMI[i] > upp_lim && upp_lim < control$signal[i]){
+      control$Intervalo[i]= 1
+    }else if(control$SMI[i] < low_lim && control$signal[i] < low_lim){
+      control$Intervalo[i]= -1
+    }else{
+      control$Intervalo[i]= 0
+    }
+  }
+  control$Int_diff=c(NA,diff(control$Intervalo))
+  
+  for(i in size){
+    
+    if((control$Intervalo[i-1] != 0 || control$Intervalo[i] != 0) && control$Cruce[i] != 0){
+      flag = control$Cruce[i]
+      #hubo cruce
+    }else{
+      warning("standby",immediate. = TRUE)
+      return('standby')
+    }
+    if(control$Int_diff[i] != 0){
+      
+      if(control$Int_diff[i] == -1 && flag == -1){
+        warning("sell",immediate. = TRUE)
+        return('sell')
+      }else if(control$Int_diff[i] == 1  && flag == 1){
+          warning("buy",immediate. = TRUE)
+          return('buy')
+      }else{warning("standby",immediate. = TRUE)
+        return('stanby')}
+    }else{warning("standby",immediate. = TRUE)
+      return('standby')}
+  }
+  #return(control)
 }
+
+
+
+Norder <-function(AccountType,AccountID,Token,OrderType,Instrument,Count,Side,Expiry, Price)
+{
+  if(AccountType == "practice"){
+    httpaccount <- "https://api-fxpractice.oanda.com"
+  } else 
+    if(AccountType == "live"){
+      httpaccount <- "https://api-fxtrade.oanda.com"
+    } else print("Account type error. Must be practice or live")
+  
+  auth       <- c(Authorization = paste("Authorization: Bearer",Token,sep=" "))
+  Queryhttp  <- paste(httpaccount,"/v1/accounts/",sep="")
+  Queryhttp1 <- paste(Queryhttp,AccountID,sep="")
+  Queryhttp2 <- paste(Queryhttp1,"/orders",sep="")
+  
+  if(OrderType == 'market'){
+    Param <- c(instrument=Instrument, units=Count, side=Side, type=OrderType)#,stopLoss=SL, takeProfit=TP, trailingStop=TS)
+  } else  if(OrderType == 'limit'){
+    Param <- c(instrument=Instrument,units=Count,side=Side,type=OrderType,
+               price=Price, expiry=Expiry, stopLoss=SL, takeProfit=TP, trailingStop=TS)
+  } else  if(OrderType == 'stop'){
+    Param <- c(instrument=Instrument,units=Count,side=Side,type=OrderType,
+               price=Price, stopLoss=SL, takeProfit=TP, expiry=Expiry, trailingStop=TS)
+  } else  if(OrderType == 'marketIfTouched'){
+    Param <- c(instrument=Instrument,units=Count,side=Side,type=OrderType,
+               price=Price, stopLoss=SL, takeProfit=TP, expiry=Expiry, trailingStop=TS)
+  } else print("Order Type error. Must be: 'market', 'limit', 'stop', 'marketIfTouched'")
+  
+  PF <- postForm(Queryhttp2, style="POST", .params=Param,
+                 .opts=list(httpheader=auth,ssl.verifypeer = FALSE))
+  InstJson <- fromJSON(PF, simplifyDataFrame = TRUE)
+  
+  return(InstJson)
+}
+
+
+HPrice<-function(AccountType,Granularity,DayAlign,TimeAlign,Token,Instrument,
+         Start,End,Count){
+  if(AccountType == "practice"){
+    httpaccount  <- "https://api-fxpractice.oanda.com"
+  } else 
+    if(AccountType == "live"){
+      httpaccount  <- "https://api-fxtrade.oanda.com"
+    } else print("Account type error. Must be practice or live")
+  
+  if(!is.null(Count)) {
+    
+    auth           <- c(Authorization = paste("Bearer",Token,sep=" "))
+    QueryHistPrec  <- paste(httpaccount,"/v1/candles?instrument=",sep="")
+    QueryHistPrec1 <- paste(QueryHistPrec,Instrument,sep="")
+    
+    qcount  <- paste("count=",Count,sep="")
+    
+    qcandleFormat <- "candleFormat=midpoint"
+    qgranularity  <- paste("granularity=",Granularity,sep="")
+    qdailyalignment    <- paste("dailyAlignment=",DayAlign,sep="")
+    
+    QueryHistPrec2 <- paste(QueryHistPrec1,qcandleFormat,qgranularity,
+                            qdailyalignment,qcount,sep="&")
+    
+  }
+  
+  else {
+    
+    auth           <- c(Authorization = paste("Bearer",Token,sep=" "))
+    QueryHistPrec  <- paste(httpaccount,"/v1/candles?instrument=",sep="")
+    QueryHistPrec1 <- paste(QueryHistPrec,Instrument,sep="")
+    
+    qstart <- paste("start=",Start,sep="")
+    qend   <- paste("end=",End,sep="")
+    
+    qcandleFormat  <- "candleFormat=midpoint"
+    qgranularity   <- paste("granularity=",Granularity,sep="")
+    qdailyalignment    <- paste("dailyAlignment=",DayAlign,sep="")
+    
+    QueryHistPrec2 <- paste(QueryHistPrec1,qstart,qend,qcandleFormat,qgranularity,
+                            qdailyalignment,sep="&")
+  }
+  
+  InstHistP <- getURL(QueryHistPrec2,cainfo=system.file("CurlSSL","cacert.pem",
+                                                        package="RCurl"),httpheader=auth)
+  InstHistPjson <- fromJSON(InstHistP, simplifyDataFrame = TRUE)
+  Prices        <- data.frame(InstHistPjson[[3]])
+  Prices$time <- paste(substr(Prices$time,1,10),substr(Prices$time,12,19), sep=" ")
+  colnames(Prices) <- c("TimeStamp","Open","High","Low","Close","TickVolume","Complete")
+  Prices$TimeStamp <- as.POSIXct(strptime(Prices$TimeStamp, "%Y-%m-%d %H:%M:%OS"),
+                                 origin="1970-01-01",tz = "UTC")
+  attributes(Prices$TimeStamp)$tzone <- TimeAlign
+  return(Prices)
+}
+
+InstPos<-function(AccountType,AccountID,Token)
+{
+  
+  if(AccountType == "practice"){
+    httpaccount  <- "https://api-fxpractice.oanda.com"
+  } else 
+    if(AccountType == "live"){
+      httpaccount <- "https://api-fxtrade.oanda.com"
+    } else print("Account type error. Must be practice or live")
+  
+  auth      <- c(Authorization = paste("Bearer",Token,sep=" "))
+  Queryhttp <- paste(httpaccount,"/v1/accounts/",sep="")
+  Querythttp1 <- paste(Queryhttp,AccountID,sep="")
+  Querythttp2 <- paste(Querythttp1,"/positions",sep="")
+  QueryInst1  <- getURL(Querythttp2,cainfo=system.file("CurlSSL","cacert.pem",
+                                                       package="RCurl"),httpheader=auth)
+  InstJson <- fromJSON(QueryInst1, simplifyDataFrame = TRUE)
+  
+  return(InstJson)
+}
+
+
+OrderHandler<-function(direction,exposure=.01){
+  ##  exposure porcentaje de margen libre por invertir.
+  ##  'check size' hay que sustituirse por el número de lotes de la operación
+  ##  Puede ser que solo funciones con lotes enteros, se recomienda redondear si da error
+  if(direction=='standby') return();
+  ask=InstPos(type,ID,token,inst)
+  if(ask$code == 14 || direction == ask$side){
+    Norder(type,ID,token,OrderType = 'market',inst,'check size',direction)
+    }else Norder(type,ID,token,OrderType = 'market',inst,2*'check size',direction)
+    
+}
+
+
+
+
+
+
+
+
+
+
+
