ba05c302cb56d4610aca895b320e01361f17a4c0
HersonG <31111841+HersonG@users.noreply.github.com>
2017-11-30 18:54:10 -0600

    Update fs.R

--- fs.R
+++ fs.R
@@ -1,4 +1,16 @@
 
+#Dicha función es nuestro modelo, el cual se basa en SMI (Stochastic Momentum Index), donde arroja señales de compra,
+#venta o de mantenerse dependiendo del cruce que tengan las medias móviles exponenciales y si cruzan en el momento siguiente los niveles 
+#establecidos como criterio de sobreventa. Se tiene el vector de SMI el cual si en la posición i es mayor que el límite superior y 
+#al mismo tiempo el límite superior es menor que la señal entonces es 1, si el vector SMI en la posición i es menor que el limite inferior
+#y la señal es menor al limite inferior entonces es menos -1, por último si no sucede ninguna de las dos es 0.
+
+#Una vez teniendo dichos valores se prosigue con un if, donde si el intervalo en la posición i-1 es diferente de cero o el intervalo
+#en la posición i es distinto de cero y el cruce en la posición i es distinto de cero entonces hubo un cruce, en ese momento se hace
+#standby y se prosigue a otro if donde si el int iff es distinto de cero entra a otro if que busca si el int_diff es igual a menos uno
+#y la vendera también entonces manda señal de venta, si las dos son uno es señal de compra, si no es ninguna de las dos se queda en 
+#standby.
+
 STR1<- function(smi,pos=1){
   control = data.frame(smi,VC=NA,Cruce=NA,Intervalo=NA,Int_diff=NA)
   control$VC=ifelse((control$SMI-control$signal)>0, 1, 0)
@@ -202,6 +214,7 @@ Acc_info<-function(AccountType,AccountID,Token)
 #OrderHandler es la función que se encarga de ejecutar las señales de compra, venta o de mantener, así como de cerrar posiciones. Dicha
 #función ejecuta opciones de compra del 15% del capital disposible, con lo cual se resuelve el problema de terminar con el capital
 #disponible, ya que se estaría creando un ciclo infinito.
+#Esta funcion se lleva a cabo de manera correcta gracias a que recibe las señales de nuestro modelo propueto.
 #Dicha función basicamente pregunta si hay operaciones abiertas, si no las hay abre la operación que indica el modelo, si existe una
 #operación pregunta cual es ese, y si resulta que es la misma  vuelve a comprar, pero si es la contraría cierra dicha operación y compra
 #o vende en ese mismo momento, dependiendo de la señal que arroje el modelo.
